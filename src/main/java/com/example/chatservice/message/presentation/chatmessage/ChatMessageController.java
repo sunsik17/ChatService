fa14921d1f77bcc77753d7c493ec3b3055a58476
sunsik17 <17sunsik@gmail.com>
2026-04-19 23:39:15 +0900

    Add `ChatMessageController` to handle WebSocket message routing for chatrooms

---
+++ src/main/java/com/example/chatservice/message/presentation/chatmessage/ChatMessageController.java
@@ -0,0 +1,23 @@
+package com.example.chatservice.message.presentation.chatmessage;
+
+import com.example.chatservice.message.presentation.chatmessage.dtos.request.Message;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class ChatMessageController {
+
+	private final SimpMessageSendingOperations messagingTemplate;
+
+	@MessageMapping("/chatrooms")
+	public void sendMessage(Message message) {
+		log.info("Received message: {}", message);
+		messagingTemplate.convertAndSend("/sub/chatrooms/" + message.chatRoomId(), message);
+	}
+
+}
